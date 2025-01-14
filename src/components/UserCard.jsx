283706ef0857b5f3d27ab6e2da03708c0f8b7bdb
ktiya23 <kashishagrahari23@gmail.com>
2025-01-14 23:13:44 +0530

    added files

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,21 +1,31 @@
-import { Button, Card, CardContent, Typography } from '@mui/material'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Button, Card, CardContent, Typography } from '@mui/material';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const UserCard = ({user}) => {
+const UserCard = ({ user }) => {
   return (
-    <Card>
-        <CardContent>
-            <Typography>{user.name}</Typography>
-            <Typography>Email:{user.email}</Typography>
-            <Typography>City:{user.address.city}</Typography>
-
-            <Button variant='contained' LinkComponent={Link} to={`/user/${user.id}`} sx={{mt:2}}>
-                View Details
-            </Button>
-        </CardContent>
+    <Card sx={{ maxWidth: 345, boxShadow: 3, borderRadius: 2 }}>
+      <CardContent>
+        <Typography variant="h6" component="div" fontWeight="bold" color="primary.main">
+          {user.name}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Email: {user.email}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          City: {user.address.city}
+        </Typography>
+        <Button
+          size="small"
+          sx={{ color: "#ff1d58" }} 
+          component={Link}
+          to={`/user/${user.id}`}
+        >
+          View Details
+        </Button>
+      </CardContent>
     </Card>
-  )
-}
+  );
+};
 
-export default UserCard
+export default UserCard;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,78 +1,112 @@
-import React, { useEffect, useState } from 'react'
-import { useContext } from 'react'
-import { UserContext } from '../context/UserContext'
-import { Alert, Button, CircularProgress, Container, Grid2, TextField, Pagination } from '@mui/material'
-import UserCard from '../components/UserCard'
+import React, { useEffect, useState, useContext } from "react";
+import { UserContext } from "../context/UserContext";
+import {
+  Alert,
+  Button,
+  CircularProgress,
+  Container,
+  Grid,
+  TextField,
+  Pagination,
+  Box,
+  Typography,
+  Grid2,
+} from "@mui/material";
+import UserCard from "../components/UserCard";
 
 const HomePage = () => {
+  const { users, setUsers, loading, setLoading, error, setError } =
+    useContext(UserContext);
+  const [filteredUsers, setFilteredUsers] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [sortOrder, setSortOrder] = useState("asc");
+  const [page, setPage] = useState(1);
 
-    const {users , setUsers , loading , setLoading , error , setError} = useContext(UserContext)
-    const [filteredUsers , setFilteredUsers] = useState([])
-    const [searchQuery , setSearchQuery] = useState("")
-    const [sortOrder , setSortOrder] = useState("asc")
-    const [page , setPage] = useState(1)
+  const Users_Per_Page = 3;
 
-    const Users_Per_Page = 5
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        const data = await response.json();
+        setUsers(data);
+        setFilteredUsers(data);
+      } catch (error) {
+        setError("Failed to fetch data");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchUsers();
+  }, [setUsers, setLoading, setError]);
 
-    useEffect(()=>{
-        const fetchUsers =  async()=>{
-            try {
-                const response = await fetch("https://jsonplaceholder.typicode.com/users")
-                const data = await response.json()
-                setUsers(data)
-                setFilteredUsers(data)
-            } catch (error) {
-                setError("Failed to fetch data")
-            } finally{
-                setLoading(false)
-            }
-        }
-        fetchUsers()
+  const handleSearch = (e) => {
+    setSearchQuery(e.target.value);
+    const filtered = users.filter((user) =>
+      user.name.toLowerCase().includes(e.target.value.toLowerCase())
+    );
+    setFilteredUsers(filtered);
+  };
 
-    } , [setUsers,setLoading,setError])
+  const handleSort = (order) => {
+    setSortOrder(order);
+    const sorted = [...filteredUsers].sort((a, b) =>
+      order === "asc"
+        ? a.name.localeCompare(b.name)
+        : b.name.localeCompare(a.name)
+    );
+    setFilteredUsers(sorted);
+  };
 
+  const paginatedUser = filteredUsers.slice(
+    (page - 1) * Users_Per_Page,
+    page * Users_Per_Page
+  );
 
-    const handleSearch = (e)=>{
-        setSearchQuery(e.target.value)
-        const filtered = users.filter(user=>user.name.toLowerCase().includes(e.target.value.toLowerCase()))
-        setFilteredUsers(filtered)
-
-    }
-
-    const handleSort=(order)=>{
-        setSortOrder(order)
-        const sorted =[...filteredUsers].sort((a,b)=>order==="asc" ? a.name.localeCompare(b.name): b.name.localeCompare(a.name))
-        setFilteredUsers(sorted)
-    }
-
-    const paginatedUser= filteredUsers.slice((page-1)*Users_Per_Page, page *Users_Per_Page)
-
-    if(loading) return <CircularProgress/>
-    if(error) return <Alert severity='error' >{error}</Alert>
+  if (loading) return <CircularProgress />;
+  if (error) return <Alert severity="error">{error}</Alert>;
   return (
-    <Container sx={{mt:10}}>
-        <TextField label="Search by name"
-        value={searchQuery}
-        onChange={handleSearch}
-        margin="normal"/>
-        <Button variant='contained' onClick={()=> handleSort("asc")}>Sort A-Z</Button>
-        <Button variant='contained' sx={{ml:2}} onClick={()=> handleSort("desc")}>Sort Z-A</Button>
-        <Grid2 container spacing={3} sx={{ mt: 2 }}>
-            {paginatedUser.map(user=>(
-                <Grid2 item xs={12} sm={6} md={4} key={user.id}>
-                    <UserCard user={user} />
-                </Grid2>
-
-            ))}
-        </Grid2>
-        <Pagination
+    <Container sx={{ mt: 10 }}>
+      <Box display="flex" justifyContent="center" alignItems="center" gap={2}>
+        <TextField
+          label="Search by name"
+          value={searchQuery}
+          onChange={handleSearch}
+          margin="normal"
+          variant="outlined"
+          sx={{ width: "300px" }}
+        />
+        <Button variant="contained" onClick={() => handleSort("asc")}>
+          Sort A-Z
+        </Button>
+        <Button
+          variant="contained"
+          sx={{ ml: 2 }}
+          onClick={() => handleSort("desc")}
+        >
+          Sort Z-A
+        </Button>
+      </Box>
+      <Typography textAlign="center" variant="h4" color="#0049B7" sx={{ mt: 2, fontWeight: "bold" }}>
+        USERS LIST
+      </Typography>
+      <Grid2 container spacing={3} sx={{ mt: 4, justifyContent: "center" }}>
+        {paginatedUser.map((user) => (
+          <Grid2 item xs={12} sm={6} md={4} key={user.id}>
+            <UserCard user={user} />
+          </Grid2>
+        ))}
+      </Grid2>
+      <Pagination
         count={Math.ceil(filteredUsers.length / Users_Per_Page)}
         page={page}
         onChange={(e, value) => setPage(value)}
         sx={{ mt: 3, display: "flex", justifyContent: "center" }}
       />
     </Container>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
